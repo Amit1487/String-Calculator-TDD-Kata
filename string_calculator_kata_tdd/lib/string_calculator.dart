7fa850b53350bf7a9a39c7a38440af0fffa44129
amit1487 <amit140787@yahoo.com>
2026-01-07 19:27:08 +0530

    Green: Added Support in split function to parse custom delimiters of any length and Update _splitNumbers to accept List<String> delimiters

--- string_calculator_kata_tdd/lib/string_calculator.dart
+++ string_calculator_kata_tdd/lib/string_calculator.dart
@@ -8,25 +8,50 @@ class StringCalculator {
 
   List<String> split(String numbers) {
     if (numbers.startsWith('//')) {
-      String delimiter = numbers[2];
-      String rest = numbers.substring(4);
-      return _splitNumbers(rest, delimiter);
+      // Find the position of the first \n after //
+      int newlineIndex = numbers.indexOf('\n');
+      if (newlineIndex == -1) return [];
+
+      String delimiterPart = numbers.substring(2, newlineIndex);
+      String rest = numbers.substring(newlineIndex + 1);
+
+      // Handle multiple delimiters like //[***][%%%]\n or single //[***]\n
+      List<String> delimiters = [];
+      int start = 0;
+      while (start < delimiterPart.length) {
+        if (delimiterPart.startsWith('[', start)) {
+          int end = delimiterPart.indexOf(']', start + 1);
+          if (end != -1) {
+            delimiters.add(delimiterPart.substring(start + 1, end));
+            start = end + 1;
+          } else {
+            break;
+          }
+        } else {
+          // Single char delimiter (backward compatibility)
+          delimiters.add(delimiterPart[start]);
+          start++;
+        }
+      }
+
+      return _splitNumbers(rest, delimiters);
     }
-    return _splitNumbers(numbers, ',');
+    return _splitNumbers(numbers, [',']);
   }
 
-  List<String> _splitNumbers(String numbers, String delimiter) {
-    return numbers
-        .replaceAll('\n', delimiter)
-        .split(delimiter)
-        .where((s) => s.isNotEmpty)
-        .toList();
+  List<String> _splitNumbers(String numbers, List<String> delimiters) {
+    String normalized = numbers.replaceAll('\n', delimiters[0]);
+    for (String delimiter in delimiters) {
+      normalized = normalized.replaceAll(delimiter, ',');
+    }
+    return normalized.split(',').where((s) => s.isNotEmpty).toList();
   }
 
   int sum(List<String> numbers) {
     var negatives = numbers.where((n) => int.parse(n) < 0);
     if (negatives.isNotEmpty) {
-      throw ArgumentError('Negatives numbers are not allowed: ${negatives.join(',')}');
+      throw ArgumentError(
+          'Negatives numbers are not allowed: ${negatives.join(',')}');
     }
 
     return numbers
