e59e8240fdddeac23f9021f1cbf25243c634b0d0
amit1487 <amit140787@yahoo.com>
2026-01-07 17:53:06 +0530

    Green: split by comma and sum the parts

--- string_calculator_kata_tdd/lib/string_calculator.dart
+++ string_calculator_kata_tdd/lib/string_calculator.dart
@@ -3,6 +3,9 @@ class StringCalculator {
   /// Adds numbers in the given string and returns the sum.
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    return int.parse(numbers);
+    if (!numbers.contains(',')) return int.parse(numbers);
+
+    var parts = numbers.split(',');
+    return int.parse(parts[0]) + int.parse(parts[1]);
   }
 }
