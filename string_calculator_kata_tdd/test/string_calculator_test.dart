58a08fe9807e1a0d1284535ae4de87cf2dc02278
amit1487 <amit140787@yahoo.com>
2026-01-07 17:15:41 +0530

    (Test) RED : Empty String Returns 0

--- string_calculator_kata_tdd/test/string_calculator_test.dart
+++ string_calculator_kata_tdd/test/string_calculator_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:string_calculator_kata_tdd/string_calculator.dart';
 
 void main() {
-
+ test('returns 0 for empty string', () {
+      expect(StringCalculator().add(''), equals(0));
+    });
 }
